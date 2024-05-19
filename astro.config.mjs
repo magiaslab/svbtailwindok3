ca6861dc41dbd01ed025a8085c3383cceb0646f9
magiaslab <social@magiaslab.com>
2024-05-19 16:59:25 +0200

    varie

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
-
+import sitemap from "@astrojs/sitemap";
+import robotsTxt from "astro-robots-txt";
 const owner = "astrojs";
+
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  site: 'https://basketsanvincenzo.it',
+  integrations: [tailwind(), sitemap(), robotsTxt()]
 });
